4021422cc7e770509c29213b8559da7e20af44d2
julio <juliomacedo.vicente@gmail.com>
2024-11-30 14:24:51 -0300

    teste

--- Docker/app/app.py
+++ Docker/app/app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, g
-import sqlite3
+import pymysql
 
 app = Flask(__name__)
 
-DATABASE = 'library.db'
+# Configurações do RDS
+RDS_CONFIG = {
+    'host': 'aaaa',
+    'user': 'aaaa',
+    'password': 'aaaa',
+    'database': 'aaaa',
+}
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
+        db = g._database = pymysql.connect(**RDS_CONFIG)
     return db
 
 @app.teardown_appcontext
@@ -24,15 +30,17 @@ def home():
 @app.route('/books')
 def books():
     db = get_db()
-    cursor = db.execute('SELECT * FROM books')
-    books = cursor.fetchall()
+    with db.cursor() as cursor:
+        cursor.execute('SELECT * FROM books')
+        books = cursor.fetchall()
     return render_template('books.html', books=books)
 
 @app.route('/authors')
 def authors():
     db = get_db()
-    cursor = db.execute('SELECT * FROM authors')
-    authors = cursor.fetchall()
+    with db.cursor() as cursor:
+        cursor.execute('SELECT * FROM authors')
+        authors = cursor.fetchall()
     return render_template('authors.html', authors=authors)
 
 if __name__ == "__main__":
